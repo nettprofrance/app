48fc7a9b11b29168c773ed8ce6acf6285e71894a
nettprofrance <nettprofrance@gmail.com>
2026-09-22 20:57:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * toujours etre fraiches.
  * Changer VERSION a chaque modification de la coquille force la mise a jour.
  */
-const VERSION = 'nettpro-coquille-v3';
+const VERSION = 'nettpro-coquille-v4';
 const FICHIERS = [
   './',
   './index.html',
@@ -14,7 +14,14 @@ const FICHIERS = [
   './icon-192.png',
   './icon-512.png',
   './icon-maskable-512.png',
-  './apple-touch-icon.png'
+  './apple-touch-icon.png',
+  './01-goutte-bleue.png',
+  './02-courbe-verte.png',
+  './03-feuille-gauche.png',
+  './04-feuille-droite.png',
+  './05-etoiles.png',
+  './06-texte-nettpro-france.png',
+  './07-slogan-proprete-multiservices.png'
 ];
 
 self.addEventListener('install', event => {
