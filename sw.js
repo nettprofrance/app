8aa9c20fa87662d69781e671e883af5189014886
nettprofrance <nettprofrance@gmail.com>
2026-09-24 13:48:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * toujours etre fraiches.
  * Changer VERSION a chaque modification de la coquille force la mise a jour.
  */
-const VERSION = 'nettpro-coquille-v10';
+const VERSION = 'nettpro-coquille-v11';
 const FICHIERS = [
   './',
   './index.html',
